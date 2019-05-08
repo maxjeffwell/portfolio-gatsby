cf3c401ec050f0f48dda6e9759e3488857bfd350
Jeff Maxwell <maxjeffwell@gmail.com>
2019-05-08 11:33:14 -0400

    add airbnb eslint config

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
+    {/* eslint-disable-next-line react/no-unescaped-entities */}
     <p>You just hit a route that doesn't exist... the sadness.</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
