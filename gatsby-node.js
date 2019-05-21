c0cbef4fe336b0f135b6a2cf9fe367dc406907b4
Jeff Maxwell <maxjeffwell@gmail.com>
2019-05-21 16:54:42 -0400

    update dependencies and fix react hot loader issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig();
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom',
+    };
+  }
+};
