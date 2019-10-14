5c9952b9074493616f268f6f204fc82a2d965c25
maxjeffwell <maxjeffwell@gmail.com>
2019-10-14 18:48:14 -0400

    justify content in header

--- src/components/header.js
+++ src/components/header.js
@@ -42,7 +42,6 @@ const Header = () => (
       css={css`
         margin-top: 0;
         grid-column: 2 / 3;
-        align-self: end;
       `}
     >
       <NavLink to="/" fontWeight="bold" activeClassName="current-page">
@@ -58,6 +57,7 @@ const Header = () => (
     <div
       css={css`
         grid-column: 3 / 4;
+        justify-content: right;
       `}
     >
       <MyLogo />
