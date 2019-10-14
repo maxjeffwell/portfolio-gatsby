374b7f73dee4f1cd3dfbf3eb345e5c42d92919ef
maxjeffwell <maxjeffwell@gmail.com>
2019-10-14 17:27:08 -0400

    remove alt attribute from site logo

--- src/components/myLogo.js
+++ src/components/myLogo.js
@@ -24,7 +24,7 @@ const MyLogo = () => (
         }
       }
     `}
-    render={data => <StyledImg fixed={data.myLogo.childImageSharp.fixed} alt="Site Logo" />}
+    render={data => <StyledImg fixed={data.myLogo.childImageSharp.fixed} />}
   />
 );
 
