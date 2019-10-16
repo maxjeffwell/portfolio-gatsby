c5f5fa945f4c24074f28335c67783b9079b0e65f
maxjeffwell <maxjeffwell@gmail.com>
2019-10-15 20:40:08 -0400

    disable angellist and linkedin links

--- src/components/layout.js
+++ src/components/layout.js
@@ -186,7 +186,7 @@ const Layout = ({ children }) => (
             `}
             target="_blank"
             rel="noopener noreferrer"
-            href="https://angel.co/maxjeffwell"
+            // href="https://angel.co/maxjeffwell"
           >
             <FaAngellist
               css={css`
@@ -207,7 +207,7 @@ const Layout = ({ children }) => (
             `}
             target="_blank"
             rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/jeffrey-maxwell-553176172"
+            // href="https://www.linkedin.com/in/jeffrey-maxwell-553176172"
           >
             <FaLinkedin
               css={css`
