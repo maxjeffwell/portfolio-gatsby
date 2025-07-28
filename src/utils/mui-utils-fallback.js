7196a728d1fb2ebfc7f69d889615bda55e2eef1f
Jeff M <maxjeffwell@gmail.com>
2025-07-28 18:11:07 -0400

    Fix SVG path error by ensuring path data is always a string
    - Modified createSvgIcon in mui-utils-fallback.js to validate path parameter
    - Added type check to ensure path is a string before passing to d attribute
    - Prevents [object Object] error in SVG path elements during SSR

--- src/utils/mui-utils-fallback.js
+++ src/utils/mui-utils-fallback.js
@@ -3,12 +3,17 @@ import React from 'react';
 
 // Simple createSvgIcon function for SSR
 const createSvgIcon = (path, displayName) => {
-  const SvgIcon = (props) => React.createElement('svg', {
-    ...props,
-    viewBox: '0 0 24 24',
-    focusable: 'false',
-    'aria-hidden': 'true',
-  }, React.createElement('path', { d: path }));
+  const SvgIcon = (props) => {
+    // Ensure path is a string, not an object
+    const pathData = typeof path === 'string' ? path : '';
+    
+    return React.createElement('svg', {
+      ...props,
+      viewBox: '0 0 24 24',
+      focusable: 'false',
+      'aria-hidden': 'true',
+    }, React.createElement('path', { d: pathData }));
+  };
   
   SvgIcon.displayName = displayName;
   return SvgIcon;
