2139e9ddc6648d9413f3b8881d0ece8b50ae4185
Jeff M <maxjeffwell@gmail.com>
2025-08-17 17:20:16 -0400

    Enable iframe embedding for iframetester.com and webofdevs.com

--- scripts/generate-csp-hashes.js
+++ scripts/generate-csp-hashes.js
@@ -78,7 +78,7 @@ function generateCSPForFiles() {
     "manifest-src 'self'",
     "frame-src *.hotjar.com *.clarity.ms 'self'",
     "object-src 'none'",
-    "frame-ancestors 'none'",
+    "frame-ancestors 'self' https://iframetester.com https://webofdevs.com",
     "base-uri 'self'",
     "form-action 'self' https://el-jefe.me",
     'upgrade-insecure-requests',
